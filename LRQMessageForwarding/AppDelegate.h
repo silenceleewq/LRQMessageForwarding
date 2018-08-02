900f7a8bf9310c18661b5dac3409425cbf2dd64f
lirenqiang <958246321@qq.com>
2018-08-02 17:44:50 +0800

    Initial Commit

---
+++ LRQMessageForwarding/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LRQMessageForwarding
+//
+//  Created by Maskkk on 2018/8/2.
+//  Copyright © 2018 lirenqiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LRQMessageForwarding/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LRQMessageForwarding
+//
+//  Created by Maskkk on 2018/8/2.
+//  Copyright © 2018 lirenqiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
